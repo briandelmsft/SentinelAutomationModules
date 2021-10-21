820f550b102c0227c508668bf631bccb966dc54a
piaudonn <piaudonn@microsoft.com>
2021-10-21 00:42:23 -0400

    update

---
+++ Modules/MDEModule/GrantAPIPermissions.ps1
@@ -0,0 +1,26 @@
+﻿#  Install-Module AzureAD # Install the module (You need admin on the machine)
+$TenantID=""  #Add your AAD Tenant Id
+$DisplayNameOfMSI="Get-MDEUserDetails" #Name of the Logic App
+
+Connect-AzureAD -TenantId $TenantID
+
+$MSI = (Get-AzureADServicePrincipal -Filter "displayName eq '$DisplayNameOfMSI'")
+Start-Sleep -Seconds 5
+$GraphAppId = "00000003-0000-0000-c000-000000000000"
+$PermissionName = "User.Read.All"
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
+
+$GraphAppId = "fc780465-2017-40d4-a0c5-307022471b92"
+$PermissionName = "AdvancedQuery.Read.All"
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
+
+$GraphAppId = "fc780465-2017-40d4-a0c5-307022471b92"
+$PermissionName = "Machine.Read.All"
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MSI.ObjectId -PrincipalId $MSI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
+
