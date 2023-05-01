b9ab9a8c62fce7c420a5aab52679a45c55add8dd
Sreedhar Ande <andesreedhar@gmail.com>
2023-05-01 12:28:13 -0700

    Added function to deploy the required modules if they are missing

--- Deploy/LegacyGrantPermissions.ps1
+++ Deploy/LegacyGrantPermissions.ps1
@@ -1,6 +1,3 @@
-#  Install-Module AzureAD -Force # Install the module (You need admin on the machine)
-#  Install-Module -Name Az -Repository PSGallery -Force  # Install the module (You need admin on the machine)
-
 #  Required Permissions
 #  To execute the Set-APIPermissions function you will need an Azure AD Global Administrator or an Azure AD Privileged Role Administrator
 #  To execute the Set-RBACPermissions function you will need either Resource Group Owner or User Access Administrator on the Microsoft Sentinel resource group
@@ -25,6 +22,9 @@ $WatchlistLogicAppName="Get-WatchlistInsights"   #Name of the Watchlists Logic A
 
 $SampleLogicAppName="Sample-STAT-Triage"      #Name of the Sample Logic App
 
+Get-RequiredModules("Az")
+Get-RequiredModules("AzureAD")
+
 Connect-AzureAD -TenantId $TenantID
 Login-AzAccount
 Set-AzContext -Subscription $AzureSubscriptionId
@@ -44,6 +44,86 @@ function Get-AppIds ($AppName) {
 function Set-RBACPermissions ($MSIName, $Role) {
     $MSI = Get-AppIds -AppName $MSIName
     New-AzRoleAssignment -ApplicationId $MSI.AppId -Scope "/subscriptions/$($AzureSubscriptionId)/resourceGroups/$($SentinelResourceGroupName)" -RoleDefinitionName $Role
+}
+
+Function Get-RequiredModules {
+    <#
+    .DESCRIPTION 
+    Get-RequiredModules is used to install and then import a specified PowerShell module.
+    
+    .PARAMETER Module
+    parameter specifices the PowerShell module to install. 
+    #>
+
+    [CmdletBinding()]
+    param (        
+        [parameter(Mandatory = $true)] $Module        
+    )
+    
+    try {
+        $installedModule = Get-InstalledModule -Name $Module -ErrorAction SilentlyContinue       
+
+        if ($null -eq $installedModule) {
+            Write-Host "The $Module PowerShell module was not found"
+            #check for Admin Privleges
+            $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+            if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+                #Not an Admin, install to current user            
+                Write-Host "Can not install the $Module module. You are not running as Administrator"
+                Write-Host "Installing $Module module to current user Scope"
+                
+                Install-Module -Name $Module -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
+                Import-Module -Name $Module -Force
+            }
+            else {
+                #Admin, install to all users																		   
+                Write-Host "Installing the $Module module to all users"
+                Install-Module -Name $Module -Repository PSGallery -Force -AllowClobber
+                Import-Module -Name $Module -Force
+            }
+        }
+        else {
+            if ($UpdateAzModules) {
+                Write-Host "Checking updates for module $Module"
+                $currentVersion = [Version](Get-InstalledModule | Where-Object {$_.Name -eq $Module}).Version
+                # Get latest version from gallery
+                $latestVersion = [Version](Find-Module -Name $Module).Version
+                if ($currentVersion -ne $latestVersion) {
+                    #check for Admin Privleges
+                    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+                    if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+                        #install to current user            
+                        Write-Host "Can not update the $Module module. You are not running as Administrator"
+                        Write-Host "Updating $Module from [$currentVersion] to [$latestVersion] to current user Scope"
+                        Update-Module -Name $Module -RequiredVersion $latestVersion -Force
+                    }
+                    else {
+                        #Admin - Install to all users																		   
+                        Write-Host "Updating $Module from [$currentVersion] to [$latestVersion] to all users"
+                        Update-Module -Name $Module -RequiredVersion $latestVersion -Force
+                    }
+                }
+                else {
+                    $latestVersion = [Version](Get-Module -Name $Module).Version               
+                    Write-Host "Importing module $Module with version $latestVersion"
+                    Import-Module -Name $Module -RequiredVersion $latestVersion -Force
+                }
+            }
+            else {                
+                # Get latest version
+                $latestVersion = [Version](Get-Module -Name $Module).Version               
+                Write-Host "Importing module $Module with version $latestVersion"
+                Import-Module -Name $Module -RequiredVersion $latestVersion -Force                
+            }
+        }
+        # Install-Module will obtain the module from the gallery and install it on your local machine, making it available for use.
+        # Import-Module will bring the module and its functions into your current powershell session, if the module is installed.  
+    }
+    catch {
+        Write-Host "An error occurred in Get-RequiredModules() method - $($_)"        
+    }
 }
 
 #UEBA
